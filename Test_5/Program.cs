2811b1c82f009676e6e10a8e30c72d1e7ce5899d
Nadya <mnpa.by@gmail.com>
2019-02-17 21:54:08 +0300

    Initial commit

---
+++ Test_5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set set = new Set();
+            set.Elements = new List<int>(new int[] { 1, 2, 3, 4 });
+            set.Count = 4;
+            Set set2 = new Set();
+            set2.Elements = new List<int>() { 1, 2, 3, 4 };
+            Set set0 = new Set();
+            set0.Elements = new List<int>() { 0, -2, -23, 5, 1 };
+            set.Add(5);
+            set0.Remove(5);
+            set0.Print();
+            Console.WriteLine("Первый список: ");
+            set.Print();
+            Console.WriteLine("Второй список: ");
+            set2.Print();
+            Set set3 = new Set();
+            Console.WriteLine("Результат объединения списков: ");
+            set3 = set.Union(set2);
+            set3.Print();
+            Console.WriteLine("Результат пересечения списков: ");
+            set3 = set.Intersect(set2);
+            set3.Print();
+            Console.WriteLine("Результат разности списков: ");
+            set3 = set.Except(set2);
+            set3.Print();
+            Console.WriteLine("Результат симметрической разности списков: ");
+            set3 = set.SymmetricDifference(set2);
+            set3.Print();
+            set2.IsSubsetOf(set);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test_5/Set.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_5
+{
+    public class Set
+    {
+
+        public List<int> Elements;
+        public int Count { get; set; }
+
+
+        public Set()
+        {
+            Elements = new List<int>();
+            Count = 0;
+        }
+
+        public void Add(int newVal)
+        {
+            bool n = false;
+            for (int j = 0; j < this.Count; j++)
+            {
+                if (this.Elements[j] == newVal)
+                {
+                    n = true;
+                }
+            }
+            if (n == false)
+            {
+                this.Elements.Add(newVal);
+            }
+        }
+
+        public void Remove(int newVal)
+        {
+            Elements.Remove(newVal);
+        }
+
+        public void Print()
+        {
+            foreach (int element in this.Elements)
+            {
+                Console.Write($"{ element }  ");
+            }
+            Console.WriteLine();
+        }
+
+
+        public Set Union(Set set2)
+        {
+            Set set3 = new Set();
+            IEnumerable<int> ElFin = this.Elements.Union(set2.Elements);
+            set3.Elements.AddRange(ElFin);
+            return set3;
+        }
+
+        public Set Intersect(Set set2)
+        {
+            Set set3 = new Set();
+            IEnumerable<int> ElFin = Elements.Intersect(set2.Elements);
+            set3.Elements.AddRange(ElFin);
+            return set3;
+        }
+
+        public Set Except(Set set2)
+        {
+            Set set3 = new Set();
+            IEnumerable<int> ElFin = Elements.Except(set2.Elements);
+            set3.Elements.AddRange(ElFin);
+            return set3;
+        }
+
+        public Set SymmetricDifference(Set set2)
+        {
+            Set set3 = new Set();
+            IEnumerable<int> ElFin = Elements.Union(set2.Elements).Except(Elements.Intersect(set2.Elements));
+            set3.Elements.AddRange(ElFin);
+            return set3;
+        }
+
+        public bool IsSubsetOf(Set set)
+        {
+
+            int count = 0;
+            for (int i = 0; i < Elements.Count; ++i)
+            {
+                for (int j = 0; j < Elements.Count; ++j)
+                {
+                    if (Elements[i] == set.Elements[j])
+                    {
+                        ++count;
+                        //Console.WriteLine("A element of the set belongs to a set");
+                    }
+                }
+            }
+
+            if (count == Elements.Count)
+            {
+                Console.WriteLine("True");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("False");
+                return false;
+            }
+        }
+
+       
+    }
+}
+
+
+
+            
+       
+ 
+
